4c428d51bd87949062537db011d7905ae33f3052
Muhammad Shahzad <shahzadthathal@gmail.com>
2021-02-01 00:53:51 +0400

    Google map added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,50 @@
+import React, { useState,useEffect } from 'react';
+
+
 import logo from './logo.svg';
 import './App.css';
+import GoogleMapReact from 'google-map-react';
 
 function App() {
+
+  const [center, setCenter]= useState( {
+    lat: 59.95,
+    lng: 30.33
+  } );
+
+  const [zoom, setZoom]= useState(11)
+
+  const [loading, setLoading]= useState(false)
+
+
+    useEffect(() => {
+      //if(!loading){
+        navigator.geolocation.getCurrentPosition(function(position) {
+          console.log("User current position")
+          console.log(position)
+          console.log("Latitude is :", position.coords.latitude);
+          console.log("Longitude is :", position.coords.longitude);
+          setCenter({ lat: position.coords.latitude, lng: position.coords.longitude})
+        });
+        setLoading(true)
+      //}
+    }, [loading])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      
+      <div style={{ height: '100vh', width: '100%' }}>
+       {loading &&  <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyDoC_LRmWwCF_LnyH1Kf1tNSddMGSn2kW4' }}
+          defaultCenter={center}
+          defaultZoom={zoom}
         >
-          Learn React
-        </a>
-      </header>
+
+        </GoogleMapReact>
+        }
+        </div>
+
     </div>
   );
 }
