e1dc6f96931569b603337b1adbc43ab56f09f348
Muhammad Shahzad <shahzadthathal@gmail.com>
2021-01-31 23:16:11 +0400

    Mongodb atlas cloud connection created,model,routes,controller for restaurants created

--- server/db/index.js
+++ server/db/index.js
@@ -1,1 +1,20 @@
 const mongoose = require('mongoose')
+
+
+
+
+let dbUrl = process.env.APP_DB || 'mongodb+srv://restaurants-near-me_user:hksY897sl@restaurants-near-me-clu.bczxm.mongodb.net/restaurants_near_me_db_mongo?retryWrites=true&w=majority'
+
+mongoose
+.connect(dbUrl, { 
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+})
+.catch(e => {
+    console.error('Connection error', e.message)
+})
+
+const db = mongoose.connection
+module.exports = db
